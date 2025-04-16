15aef495a33cb8b6515919912804c994c3574b1f
Assiar1 <voiceilyas@gmail.com>
2025-04-16 09:08:07 +0100

    vesrion de scrum master

---
+++ CI-CD.java
@@ -0,0 +1,8 @@
+public class BonjourMonde {
+    public static void main(String[] args) {
+        System.out.println("Bonjour, tous le monde !");
+    
+
+        System.out.println("première version pushed par le Scrum Master");
+    }
+}
